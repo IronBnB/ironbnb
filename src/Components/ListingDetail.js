078848e3bb156bb242f7b21f830fff090bab8ea4
robbiesanchez <rfsanchez004@gmail.com>
2019-11-22 15:31:47 -0500

    s

--- src/Components/ListingDetail.js
+++ src/Components/ListingDetail.js
@@ -10,7 +10,7 @@ export default class ListingDetail extends Component {
             <div className="container-fluid">
             <div className="row">
             <div className="col-6">
-                <img src={this.props.img}></img>
+                <img src={this.props.img} alt=""></img>
             </div>
             <div className="col-6">
             <div>Title: {this.props.title}</div>
