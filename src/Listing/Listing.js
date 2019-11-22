1bf703fc83035525af8b8913188b2e5039b3a47e
avrahm <me@avrahm.com>
2019-11-22 14:08:05 -0500

    listing

---
+++ src/Listing/Listing.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Listing extends Component {
+
+    showListing = () => {
+        return this.props.listingData.map(( eachListing,i)=> {
+          return (
+            <div className="listing" key={i}>
+            <h3>{eachListing.title}</h3>
+            <h5>{eachListing.link}</h5>
+            <p></p>
+            </div>
+          )
+        })
+      }
+    render() {
+        return (
+            <div>
+                
+        {this.showListing()}
+            </div>
+        )
+    }
+}
