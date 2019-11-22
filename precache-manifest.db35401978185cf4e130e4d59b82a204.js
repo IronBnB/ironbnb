623ca1940b34dd0a0b7efed011d37030f9edba0f
avrahm <me@avrahm.com>
2019-11-22 13:33:01 -0500

    Updates

---
+++ precache-manifest.db35401978185cf4e130e4d59b82a204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a81c70f1fcf01b21323e9691731107",
+    "url": "/IronBnB/ironbnb/index.html"
+  },
+  {
+    "revision": "360ede28ce5bca5ad688",
+    "url": "/IronBnB/ironbnb/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "392419e1ea9a746bdb4e",
+    "url": "/IronBnB/ironbnb/static/js/2.95c430ac.chunk.js"
+  },
+  {
+    "revision": "360ede28ce5bca5ad688",
+    "url": "/IronBnB/ironbnb/static/js/main.9d69cb8c.chunk.js"
+  },
+  {
+    "revision": "e104e3db9fafe1b68675",
+    "url": "/IronBnB/ironbnb/static/js/runtime-main.dee63a36.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/IronBnB/ironbnb/static/media/logo.25bf045c.svg"
+  }
+]);
